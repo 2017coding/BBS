dc468bf7639c1154f52c2b7d9c2dbe12d2085070
lyh <1695304848@qq.com>
2019-06-28 14:16:10 +0800

    配置cdn，gzip等进行首屏优化

--- vue.config.js
+++ vue.config.js
@@ -18,8 +18,8 @@ module.exports = {
   outputDir: 'bbs',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
   devServer: {
-    host: 'localhost',
     port: 9005,
     open: true,
     overlay: {
@@ -36,6 +36,21 @@ module.exports = {
     }
   },
   chainWebpack: config => {
+    config.plugins.delete('preload')
+    config.plugins.delete('prefetch')
+    // 忽略的打包文件
+    config.externals({
+      'vue': 'Vue',
+      'vue-router': 'VueRouter',
+      'vuex': 'Vuex',
+      'axios': 'axios',
+      'element-ui': 'ELEMENT',
+      "echarts": 'echarts'
+    })
+    const entry = config.entry('app')
+    entry
+      .add('babel-polyfill')
+      .end()
     //修改文件引入自定义路径
     config.resolve.alias
       .set('@', resolve('src'))
@@ -49,5 +64,73 @@ module.exports = {
         bypassOnDebug: true
       })
       .end()
+    // set preserveWhitespace
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options.compilerOptions.preserveWhitespace = true
+        return options
+      })
+      .end()
+    config
+      // https://webpack.js.org/configuration/devtool/#development
+      .when(process.env.NODE_ENV === 'development',
+        config => config.devtool('cheap-source-map')
+      )
+    config
+      .when(process.env.NODE_ENV !== 'development',
+        config => {
+          config
+            .plugin('ScriptExtHtmlWebpackPlugin')
+            .after('html')
+            .use('script-ext-html-webpack-plugin', [{
+            // `runtime` must same as runtimeChunk name. default is `runtime`
+              inline: /runtime\..*\.js$/
+            }])
+            .end()
+          config
+            .optimization.splitChunks({
+              chunks: 'all',
+              cacheGroups: {
+                libs: {
+                  name: 'chunk-libs',
+                  test: /[\\/]node_modules[\\/]/,
+                  priority: 10,
+                  chunks: 'initial' // only package third parties that are initially dependent
+                },
+                elementUI: {
+                  name: 'chunk-elementUI', // split elementUI into a single package
+                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                },
+                commons: {
+                  name: 'chunk-commons',
+                  test: resolve('src/components'), // can customize your rules
+                  minChunks: 3, //  minimum common number
+                  priority: 5,
+                  reuseExistingChunk: true
+                }
+              }
+            })
+          config.optimization.runtimeChunk('single')
+        }
+      )
+      if (config.productionGzip) {
+        const CompressionWebpackPlugin = require('compression-webpack-plugin');
+        //增加浏览器CPU（需要解压缩）， 减少网络传输量和带宽消耗 （需要衡量，一般小文件不需要压缩的）
+        //图片和PDF文件不应该被压缩，因为他们已经是压缩的了，试着压缩他们会浪费CPU资源而且可能潜在增加文件大小。
+        webpackConfig.plugins.push(
+            new CompressionWebpackPlugin({
+                asset: '[path].gz[query]',
+                algorithm: 'gzip',
+                test: /\.(js|css)$/,
+                threshold: 10240,//达到10kb的静态文件进行压缩 按字节计算
+                minRatio: 0.8,//只有压缩率比这个值小的资源才会被处理
+                deleteOriginalAssets: false//使用删除压缩的源文件
+            })
+        )
+    }
   }
 }
